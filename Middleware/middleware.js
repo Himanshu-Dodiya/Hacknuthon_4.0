a178b5c73680fb82645c8deab3d4ac9beb1733f2
Manav <200170116034@vgec1.gujgov.edu.in>
2023-04-29 16:40:06 +0530

    controller integration

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -1,21 +1,4 @@
-// fetch api using localhost:2000/name
-
-// Path: Middleware\middleware.js
-
 const graphql_client = require('graphql-client');
-const client = graphql_client({
-    url: 'http://localhost:2000/name',
-});
-
-const output = client.query(`
-    query {
-        name {
-            name
-        }
-    }
-`).then((data) => {
-    console.log(data);
-});
+const controller = require('controller');
 
 
-console.log("got data"  + output);
