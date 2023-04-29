69aa326d26670f4d7183e47c0a63b36e2d95da85
Manav <200170116034@vgec1.gujgov.edu.in>
2023-04-29 17:17:12 +0530

    final comit

--- Middleware/middleware.js
+++ Middleware/middleware.js
@@ -1,11 +1,13 @@
 const graphql_client = require('graphql-client');
 const controller = require('../warehouse/controller/controller');
 
-const out = controller.addProduct({product_id:"1" ,product_name: "test", product_quantity: 10, product_price: 10 });
+const router = express.router();
 
-if(out){
-    console.log("data entered");
-}else{
-    console.log("data not entered")
-}
+// const out = controller.addProduct({product_id:"1" ,product_name: "test", product_quantity: 10, product_price: 10 });
+
+// if(out){
+//     console.log("data entered");
+// }else{
+//     console.log("data not entered")
+// }
 
